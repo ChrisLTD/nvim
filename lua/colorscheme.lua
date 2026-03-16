8280a478c6f05d0d6ae307b2d1fae00e580bb9a9
Chris Johnson <chris@measured.fi>
2026-03-16 12:14:53 -0400

    auto dark/light

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,6 +1,6 @@
 -- Set theme
 -- Comment out the line below to allow auto light/dark mode
-vim.opt.background = "dark"
+-- vim.opt.background = "dark"
 vim.cmd("colorscheme cyberdream")
 -- vim.cmd("colorscheme oxocarbon")
 -- vim.cmd("colorscheme rose-pine")
