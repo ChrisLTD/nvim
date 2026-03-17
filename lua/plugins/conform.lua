184b1d8d2f5eca40c9aa74ca7b75325f24709e99
Chris Johnson <chris@measured.fi>
2026-03-17 14:09:50 -0400

    fixed conform config for tsx and jsx files:

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,7 +10,9 @@ return {
 			formatters_by_ft = {
 				go = { "gofmt" },
 				javascript = { "prettier", "eslint_d" },
+				javascriptreact = { "prettier", "eslint_d" },
 				typescript = { "prettier", "eslint_d" },
+				typescriptreact = { "prettier", "eslint_d" },
 			}
 		})
 
