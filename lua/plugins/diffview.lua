33c7ef1502121ce6a5c6da14abd1e1b1dc85c069
Chris Johnson <chris@measured.fi>
2026-04-23 14:45:26 -0400

    Changed diffview keybindings from <leader>gv to <leader>D for faster access

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,10 +2,10 @@ return {
 	"sindrets/diffview.nvim",
 	cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
 	config = function()
-		vim.keymap.set("n", "<leader>gvo", "<cmd>DiffviewOpen<cr>", { desc = "Diffview open" })
-		vim.keymap.set("n", "<leader>gvp", "<cmd>DiffviewOpen origin/main... --imply-local<cr>", { desc = "Diffview PR review (vs origin/main)" })
-		vim.keymap.set("n", "<leader>gvc", "<cmd>DiffviewClose<cr>", { desc = "Diffview close" })
-		vim.keymap.set("n", "<leader>gvh", "<cmd>DiffviewFileHistory %<cr>", { desc = "Diffview file history (current file)" })
-		vim.keymap.set("n", "<leader>gvH", "<cmd>DiffviewFileHistory<cr>", { desc = "Diffview file history (repo)" })
+		vim.keymap.set("n", "<leader>Do", "<cmd>DiffviewOpen<cr>", { desc = "Diffview open" })
+		vim.keymap.set("n", "<leader>Dp", "<cmd>DiffviewOpen origin/main... --imply-local<cr>", { desc = "Diffview PR review (vs origin/main)" })
+		vim.keymap.set("n", "<leader>Dc", "<cmd>DiffviewClose<cr>", { desc = "Diffview close" })
+		vim.keymap.set("n", "<leader>Dh", "<cmd>DiffviewFileHistory %<cr>", { desc = "Diffview file history (current file)" })
+		vim.keymap.set("n", "<leader>DH", "<cmd>DiffviewFileHistory<cr>", { desc = "Diffview file history (repo)" })
 	end,
 }
